b78257341ef55eb42012fcfb5c2ed006b4dc9045
Tiina Turban <tiina303@gmail.com>
2023-02-21 16:06:00 +0100

    fix: person constraint in person overrides table (#14319)

--- posthog/models/test/test_person_override_model.py
+++ posthog/models/test/test_person_override_model.py
@@ -1,6 +1,5 @@
 import contextlib
 import datetime as dt
-from threading import Thread
 from uuid import uuid4
 
 import pytest
@@ -305,43 +304,50 @@ def test_person_override_allows_override_person_id_as_old_person_id_in_different
     assert p1.team != p2.team
 
 
-@pytest.mark.django_db(transaction=True)
-def test_person_override_disallows_old_person_id_as_override_person_id_race_conditions():
-    """Test a new override_person_id cannot match an existing old_person_id.
-
-    We re-use the old_person_id from the first model created as the override_person_id
-    of the second model. We expect an exception on saving this second model.
-
-    Note that to test the race condition scenario we need to:
-
-     1. create multiple concurrent transactions, such that we can verify
-        constraints are enforced at COMMIT time.
-     2. enable transactions for the Django test. This is more so we can see data
-        from the main Django PostgreSQL connection session in the other
-        concurrent transactions. Not 100% required but makes things a little
-        easier to write.
-    """
+def test_person_override_must_exist_in_person_table():
+    """This is guaranteed by the foreign key constraint."""
     organization = create_organization(name="test")
     team = create_team(organization=organization)
 
     oldest_event = dt.datetime.now(dt.timezone.utc)
-    old_person_id = uuid4()
+    person_id = uuid4()
+
+    Person.objects.create(
+        team_id=team.pk,
+        uuid=person_id,
+    )
+
+    with pytest.raises(IntegrityError):
+        PersonOverride.objects.create(
+            team=team,
+            old_person_id=person_id,
+            override_person_id=person_id,
+            oldest_event=oldest_event,
+            version=0,
+        ).save()
+
+
+def test_person_override_allows_duplicate_override_person_id():
+    """Test duplicate override_person_ids with different old_person_ids are allowed."""
+    organization = create_organization(name="test")
+    team = create_team(organization=organization)
+
+    oldest_event = dt.datetime.now(dt.timezone.utc)
     override_person_id = uuid4()
-    new_override_person_id = uuid4()
+    n_person_overrides = 2
+    created = []
 
-    Person.objects.create(uuid=old_person_id, team=team)
     Person.objects.create(uuid=override_person_id, team=team)
-    Person.objects.create(uuid=new_override_person_id, team=team)
 
-    with create_connection() as first_cursor, create_connection() as second_cursor:
-        first_cursor.execute("BEGIN")
-        second_cursor.execute("BEGIN")
+    for _ in range(n_person_overrides):
+        old_person_id = uuid4()
 
-        _merge_people(team, first_cursor, old_person_id, override_person_id, oldest_event)
-
-        second_merge_thread = Thread(
-            target=_merge_people_and_commit,
-            args=(team, second_cursor, override_person_id, new_override_person_id, oldest_event),
+        person_override = PersonOverride.objects.create(
+            team=team,
+            old_person_id=old_person_id,
+            override_person_id=override_person_id,
+            oldest_event=oldest_event,
+            version=1,
         )
 
         assert p1.override_person_id.uuid == p2.old_person_id.uuid
@@ -367,29 +373,10 @@ def test_person_override_disallows_old_person_id_as_override_person_id_race_cond
 
         first_cursor.execute("COMMIT")
 
-        with pytest.raises(IntegrityError):
-            first_cursor.execute("COMMIT")
+        created.append(person_override)
 
-        second_merge_thread.join()
-
-        assert list(PersonOverride.objects.all().values_list("old_person_id", "override_person_id")) == [
-            (override_person_id, new_override_person_id),
-        ]  # type: ignore
-
-        # We got an IntegrityError, so the first transaction was rolled back. We'll
-        # need to try this transaction again to get to the state we expect.
-        first_cursor.execute("BEGIN")
-        _merge_people(team, first_cursor, old_person_id, override_person_id, oldest_event)
-        first_cursor.execute("COMMIT")
-
-        mappings = list(PersonOverride.objects.all().values_list("old_person_id", "override_person_id"))
-
-        assert sorted(mappings) == sorted(
-            [
-                (override_person_id, new_override_person_id),
-                (old_person_id, new_override_person_id),
-            ]
-        ), f"{mappings=} {old_person_id=}, {override_person_id=}, {new_override_person_id=}"  # type: ignore
+    assert all(p.override_person_id == override_person_id for p in created)
+    assert len(set(p.old_person_id for p in created)) == n_person_overrides
 
 
 def test_person_override_old_person_id_as_override_person_id_in_different_teams():
@@ -436,20 +423,78 @@ def test_person_override_old_person_id_as_override_person_id_in_different_teams(
     assert p1.team != p2.team
 
 
-def test_person_override_allows_duplicate_override_person_id():
-    """Test duplicate override_person_ids with different old_person_ids are allowed."""
+@pytest.mark.django_db(transaction=True)
+def test_person_deletion_disallowed_when_override_exists():
+    """Person deletion would result in an error if the override exists
+    TODO: fix all person deletions
+    """
     organization = create_organization(name="test")
     team = create_team(organization=organization)
 
     oldest_event = dt.datetime.now(dt.timezone.utc)
+    old_person_id = uuid4()
     override_person_id = uuid4()
-    n_person_overrides = 2
-    created = []
 
+    override_person = Person.objects.create(
+        team_id=team.pk,
+        uuid=override_person_id,
+    )
+    PersonOverride.objects.create(
+        team=team,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
+        oldest_event=oldest_event,
+        version=0,
+    ).save()
+
+    with pytest.raises(IntegrityError):
+        override_person.delete()
+
+
+"""
+Concurrency tests:
+- there are two cases that we want to check for
+    - concurrent merges
+    - concurrent merge and person deletion
+
+In both cases one of the transactions will wait on the lock,
+so they can only complete in one order (the second one failing).
+TODO: Tomas to update the comment
+
+Tests:
+concurrency between:
+- 2 merges
+    - the later started transaction finishes first
+- merge & real person delete (with deletes overrides)
+    - merge starts - person deletion runs fully - merge finishes
+    - person d starts - merge finishes fully - person d finishes
+"""
+
+
+@pytest.mark.django_db(transaction=True)
+def test_concurrent_delete_first_then_merge_fails():
+    # Can't do concurrently the other order due to waiting on the lock forever
+    organization = create_organization(name="test")
+    team = create_team(organization=organization)
+
+    oldest_event = dt.datetime.now(dt.timezone.utc)
+    old_person_id = uuid4()
+    override_person_id = uuid4()
+
+    Person.objects.create(uuid=old_person_id, team=team)
     Person.objects.create(uuid=override_person_id, team=team)
+    with create_connection() as merge_cursor, create_connection() as delete_cursor:
+        # each transaction gets a "copy" of the DB state
+        merge_cursor.execute("BEGIN")
+        delete_cursor.execute("BEGIN")
+        # merge and delete
+        _merge_people(team, merge_cursor, old_person_id, override_person_id, oldest_event)
+        _delete_person(team, delete_cursor, override_person_id)
 
-    for _ in range(n_person_overrides):
-        old_person_id = uuid4()
+        # finish delete first, then merge fails
+        delete_cursor.execute("COMMIT")
+        with pytest.raises(IntegrityError):
+            merge_cursor.execute("COMMIT")
 
         old_helper = PersonOverrideHelper.objects.create(
             team=self.team,
