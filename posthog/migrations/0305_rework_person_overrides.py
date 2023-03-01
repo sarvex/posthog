6dc411a7f994dbee5f4c20daa0d5097a455d84df
Tiina Turban <tiina303@gmail.com>
2023-03-01 20:14:43 +0100

    remove model changes

--- posthog/migrations/0305_rework_person_overrides.py
+++ posthog/migrations/0305_rework_person_overrides.py
@@ -1,17 +1,10 @@
-from django.db import migrations, models
+from django.db import migrations
 
 
-# no-op change
 class Migration(migrations.Migration):
 
     dependencies = [
         ("posthog", "0304_store_dashboard_template_in_db"),
     ]
 
-    operations = [
-        migrations.AddField(
-            model_name="team",
-            name="session_recording_version_2",
-            field=models.CharField(blank=True, max_length=24, null=True),
-        ),
-    ]
+    operations = []  # type: ignore
--- posthog/models/team/team.py
+++ posthog/models/team/team.py
@@ -134,7 +134,6 @@ class Team(UUIDClassicModel):
     capture_console_log_opt_in: models.BooleanField = models.BooleanField(null=True, blank=True)
     capture_performance_opt_in: models.BooleanField = models.BooleanField(null=True, blank=True)
     session_recording_version: models.CharField = models.CharField(null=True, blank=True, max_length=24)
-    session_recording_version_2: models.CharField = models.CharField(null=True, blank=True, max_length=24)
     signup_token: models.CharField = models.CharField(max_length=200, null=True, blank=True)
     is_demo: models.BooleanField = models.BooleanField(default=False)
     access_control: models.BooleanField = models.BooleanField(default=False)
